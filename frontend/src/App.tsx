75b6d830e3886a63a51fed37269de30f7db06890
vatsalintech <workwithvatsalshah@gmail.com>
2026-02-15 17:20:35 -0500

    Basic profile structure and settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import LandingPage from "./components/landingPage/landing_page";
+import ProfilePage from "./components/profilePage/profile_page";
 
 function App() {
-  return <LandingPage />;
+  return <ProfilePage/>
+  // return <LandingPage />;
 }
 
 export default App;
