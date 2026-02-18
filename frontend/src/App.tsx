2eebb1d5ca49f4d9130d36b04689b8598c68a6d2
vatsalintech <workwithvatsalshah@gmail.com>
2026-02-17 23:26:48 -0500

    Connected profile page with landing page after successful sign-in, added logout functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Signup from './components/authenticate/Signup'
 import Signin from './components/authenticate/Signin'
 import LandingPage from './components/landingPage/landing_page'
+import ProfilePage from './components/profilePage/profile_page'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
