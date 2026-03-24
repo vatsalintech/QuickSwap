f3ed594e462d8c8982c8dd9530c746236f435037
Rukaiya Khan <rukaiyak2000@gmail.com>
2026-03-24 15:39:03 -0400

    added auction page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -20,6 +20,8 @@ func NewRouter(c *auth.Client, pg *pgxpool.Pool, rdb *redis.Client) http.Handler
 	// Register listing route
 	mux.HandleFunc("/api/createlisting", createListingHandler(c))
 	mux.HandleFunc("/api/mylistings", myListingHandler(c))
+	mux.HandleFunc("/api/listing", singleListingHandler(c))
+
 	// Register bids Api
 	mux.HandleFunc("/api/mybids", myBidsHandler(c))
 	mux.HandleFunc("/api/toplistings", topListingsHandler(c))
